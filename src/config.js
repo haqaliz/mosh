820996cdc6a8d56e6a1d73554203cf0a922bcff7
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-01-10 15:10:14 +0330

    fix: db connection error using express router
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const { Deta } = require('deta');
 
 const DEV = process.env.NODE_ENV === 'development';
-const deta = Deta(DEV && 'b0syngdn_SvdzZ6d2p1YLUWBf4HazfaoCJwDmu3ee');
+const deta = Deta('b0syngdn_SvdzZ6d2p1YLUWBf4HazfaoCJwDmu3ee');
 const db = deta.Base('mosh');
 
 module.exports = {
